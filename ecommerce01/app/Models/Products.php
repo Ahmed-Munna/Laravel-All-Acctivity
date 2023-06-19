a511c3d47caf2799f0591595b74e1d5dc2c1ab6c
Ahmed-Munna <ahmedmunna4200@gmail.com>
2023-06-19 07:16:44 +0600

    laravel project

---
+++ ecommerce01/app/Models/Products.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'subcategory_id',
+        'childcategory_id',
+        'brand_id',
+        'product_name',
+        'product_code',
+        'unit',
+        'tags',
+        'video',
+        'purchase_price',
+        'selling_price',
+        'discount_price',
+        'stock_quantity',
+        'warehouse',
+        'thumbnail',
+        'images',
+        'featured',
+        'today_deal',
+        'status',
+        'flash_deal_id',
+        'cash_on_delivery',
+    ];
+}
